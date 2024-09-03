472184f1c2e3c4db2dfb1167404ce2a14f3fde17
Devanshu254 <devanshujha8305@gmail.com>
2024-09-03 20:41:19 +0530

    all services methods and controller methods are created, controller methods are tested also

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { AfterInsert, AfterRemove, AfterUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class User {
@@ -10,4 +10,19 @@ export class User {
 
     @Column()
     password: string;
+
+    @AfterInsert()
+    logInsert() {
+        console.log("User is inserted with id ", this.id); 
+    }
+
+    @AfterUpdate()
+    logUpdate() {
+        console.log("User is updated with id ", this.id);
+    }
+
+    @AfterRemove() 
+    logRemove() {
+        console.log("User is removed with id ", this.id);
+    }
 }
