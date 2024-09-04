bdabaa3d2ee3378ac34b04e89d782f03fc5d0861
Devanshu254 <devanshujha8305@gmail.com>
2024-09-04 18:59:43 +0530

    authenticatin 02 signin method is executed

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -17,6 +17,11 @@ export class UsersController {
         return this.authService.singup(body.email, body.password);
     }
 
+    @Post('/signin')
+    signin(@Body() body: CreateUserDto) {
+        return this.authService.signin(body.email, body.password);
+    }
+
     // @UseInterceptors(new SerializeInterceptor(UserDto))
     // @Serialize(UserDto)
     @Get('/:id')
